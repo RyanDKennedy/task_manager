3b824a9d56b2300345922cf6c3e35ba3ce240b76
RyanDKennedy <kennedyryan725@outlook.com>
2025-03-18 10:21:38 -0400

    added gabe's gui

--- src/gui.py
+++ src/gui.py
@@ -10,6 +10,7 @@ class GUI():
     def __init__(self):
 
         self.root = tk.Tk()
+        self.root.geometry("1000x1000")
 
         self.db = Database()
 
@@ -19,8 +20,11 @@ class GUI():
         self.books_records = self.db.books_get_all_records()
         self.books_selected_record_index = 0
 
-        self.feedback_lbl = ttk.Label(self.root, text="")
-        self.feedback_lbl.pack()
+        self.canvas = tk.Canvas(self.root, width=1000, height=1000)
+        self.canvas.pack()
+
+        self.feedback_lbl = ttk.Label(self.root, text="", font=("Helvetica", 14))
+        self.feedback_lbl.place(x=500, y=10, anchor="center")
 
         self.authors_create_gui_elements()
         self.books_create_gui_elements()
@@ -31,54 +35,57 @@ class GUI():
 
 
     def authors_create_gui_elements(self):
-        
-        # record display
-        self.authors_id_lbl = ttk.Label(self.root, text="ID:")
-        self.authors_id_dpy = ttk.Label(self.root, text="")
-        self.authors_id_lbl.pack()
-        self.authors_id_dpy.pack()
 
-        self.authors_name_lbl = ttk.Label(self.root, text="Name:")
-        self.authors_name_ent = ttk.Entry(self.root)
-        self.authors_name_lbl.pack()
-        self.authors_name_ent.pack()
+        self.authors_display_lbl = ttk.Label(self.root, text="Authors:", font=("Helvetica", 18))
+        self.authors_display_lbl.place(x=500,y=35)
+        self.canvas.create_rectangle(300, 75, 800, 375, outline="black", width=3)
+        # record display
+        self.authors_id_lbl = ttk.Label(self.root, text="ID:", font=("Helvetica", 14))
+        self.authors_id_dpy = ttk.Label(self.root, text="", font=("Helvetica", 14))
+        self.authors_id_lbl.place(x=475,y=130)
+        self.authors_id_dpy.place(x=525,y=130)
+
+        self.authors_name_lbl = ttk.Label(self.root, text="Name:", font=("Helvetica", 14))
+        self.authors_name_ent = ttk.Entry(self.root, width=20 )
+        self.authors_name_lbl.place(x=460, y=160)
+        self.authors_name_ent.place(x=540, y=160)
         
         self.authors_birth_year_lbl = ttk.Label(self.root, text="Birth Year:")
-        self.authors_birth_year_ent = ttk.Entry(self.root)
-        self.authors_birth_year_lbl.pack()
-        self.authors_birth_year_ent.pack()
+        self.authors_birth_year_ent = ttk.Entry(self.root, width=20 )
+        self.authors_birth_year_lbl.place(x=460, y=190)
+        self.authors_birth_year_ent.place(x=540, y=190)
 
 
         # navigation buttons
         
         self.authors_back_all_btn = ttk.Button(self.root, text="|<", command=lambda: self.authors_increment_selected_record_index(-self.authors_selected_record_index))
-        self.authors_back_all_btn.pack()
+        self.authors_back_all_btn.place(x=340, y=220)
 
         self.authors_back_5_btn = ttk.Button(self.root, text="<<", command=lambda: self.authors_increment_selected_record_index(-5))
-        self.authors_back_5_btn.pack()
+        self.authors_back_5_btn.place(x=410, y=220)
 
         self.authors_back_1_btn = ttk.Button(self.root, text="<", command=lambda: self.authors_increment_selected_record_index(-1))
-        self.authors_back_1_btn.pack()
+        self.authors_back_1_btn.place(x=480, y=220)
 
         self.authors_next_1_btn = ttk.Button(self.root, text=">", command=lambda: self.authors_increment_selected_record_index(1))
-        self.authors_next_1_btn.pack()
+        self.authors_next_1_btn.place(x=550, y=220)
 
         self.authors_next_5_btn = ttk.Button(self.root, text=">>", command=lambda: self.authors_increment_selected_record_index(5))
-        self.authors_next_5_btn.pack()
+        self.authors_next_5_btn.place(x=620, y=220)
 
         self.authors_next_all_btn = ttk.Button(self.root, text=">|", command=lambda: self.authors_increment_selected_record_index(len(self.authors_records) - self.authors_selected_record_index))
-        self.authors_next_all_btn.pack()
+        self.authors_next_all_btn.place(x=690, y=220)
 
         # CRUD
 
         self.authors_insert_btn = ttk.Button(self.root, text="Insert", command=self.authors_insert_record)
-        self.authors_insert_btn.pack()
+        self.authors_insert_btn.place(x=430, y=250)
 
         self.authors_update_btn = ttk.Button(self.root, text="Update", command=self.authors_update_current_record)
-        self.authors_update_btn.pack()
+        self.authors_update_btn.place(x=500, y=250)
 
         self.authors_delete_btn = ttk.Button(self.root, text="Delete", command=self.authors_delete_current_record)
-        self.authors_delete_btn.pack()
+        self.authors_delete_btn.place(x=570,y=250)
 
 
     def authors_increment_selected_record_index(self, amt):
@@ -153,73 +160,76 @@ class GUI():
     # ==== Books Stuff ====
 
     def books_create_gui_elements(self):
-        
+
+        self.books_display_lbl = ttk.Label(self.root, text="Books:", font=("Helvetica", 18))
+        self.books_display_lbl.place(x=500,y=540)
+        self.canvas.create_rectangle(300, 575, 800, 875, outline="black", width=3)
         # record display
-        self.books_id_lbl = ttk.Label(self.root, text="ID:")
-        self.books_id_dpy = ttk.Label(self.root, text="")
-        self.books_id_lbl.pack()
-        self.books_id_dpy.pack()
+        self.books_id_lbl = ttk.Label(self.root, text="ID:", font=("Helvetica", 14))
+        self.books_id_dpy = ttk.Label(self.root, text="", font=("Helvetica", 14))
+        self.books_id_lbl.place(x=475,y=590)
+        self.books_id_dpy.place(x=525,y=590)
 
-        self.books_name_lbl = ttk.Label(self.root, text="Name:")
-        self.books_name_ent = ttk.Entry(self.root)
-        self.books_name_lbl.pack()
-        self.books_name_ent.pack()
+        self.books_name_lbl = ttk.Label(self.root, text="Name:", font=("Helvetica", 14))
+        self.books_name_ent = ttk.Entry(self.root, width=20 )
+        self.books_name_lbl.place(x=460, y=620)
+        self.books_name_ent.place(x=540, y=620)
 
-        self.books_year_released_lbl = ttk.Label(self.root, text="Year Released:")
-        self.books_year_released_ent = ttk.Entry(self.root)
-        self.books_year_released_lbl.pack()
-        self.books_year_released_ent.pack()
+        self.books_year_released_lbl = ttk.Label(self.root, text="Published Year:", font=("Helvetica", 14))
+        self.books_year_released_ent = ttk.Entry(self.root, width=20 )
+        self.books_year_released_lbl.place(x=430, y=650)
+        self.books_year_released_ent.place(x=580, y=650)
 
-        self.books_page_amt_lbl = ttk.Label(self.root, text="Page Amount:")
-        self.books_page_amt_ent = ttk.Entry(self.root)
-        self.books_page_amt_lbl.pack()
-        self.books_page_amt_ent.pack()
+        self.books_page_amt_lbl = ttk.Label(self.root, text="# of Pages:", font=("Helvetica", 14))
+        self.books_page_amt_ent = ttk.Entry(self.root, width=20 )
+        self.books_page_amt_lbl.place(x=430, y=680)
+        self.books_page_amt_ent.place(x=580, y=680)
 
-        self.books_price_lbl = ttk.Label(self.root, text="Price:")
-        self.books_price_ent = ttk.Entry(self.root)
-        self.books_price_lbl.pack()
-        self.books_price_ent.pack()
+        self.books_price_lbl = ttk.Label(self.root, text="Price:", font=("Helvetica", 14))
+        self.books_price_ent = ttk.Entry(self.root, width=20 )
+        self.books_price_lbl.place(x=460, y=710)
+        self.books_price_ent.place(x=540, y=710)
 
-        self.books_author_lbl = ttk.Label(self.root, text="Author:")
+        self.books_author_lbl = ttk.Label(self.root, text="Author:", font=("Helvetica", 14))
         self.books_author_val = tk.StringVar()
         self.books_author_cbx = ttk.Combobox(self.root, textvariable=self.books_author_val)
         self.books_author_cbx["values"] = [""]
         self.books_author_cbx["state"] = "readonly"
         self.books_author_val.set("")
-        self.books_author_lbl.pack()
-        self.books_author_cbx.pack()
+        self.books_author_lbl.place(x=460, y=740)
+        self.books_author_cbx.place(x=540, y=740)
         
 
         # navigation buttons
         
         self.books_back_all_btn = ttk.Button(self.root, text="|<", command=lambda: self.books_increment_selected_record_index(-self.books_selected_record_index))
-        self.books_back_all_btn.pack()
+        self.books_back_all_btn.place(x=340, y=770)
 
         self.books_back_5_btn = ttk.Button(self.root, text="<<", command=lambda: self.books_increment_selected_record_index(-5))
-        self.books_back_5_btn.pack()
+        self.books_back_5_btn.place(x=410, y=770)
 
         self.books_back_1_btn = ttk.Button(self.root, text="<", command=lambda: self.books_increment_selected_record_index(-1))
-        self.books_back_1_btn.pack()
+        self.books_back_1_btn.place(x=480, y=770)
 
         self.books_next_1_btn = ttk.Button(self.root, text=">", command=lambda: self.books_increment_selected_record_index(1))
-        self.books_next_1_btn.pack()
+        self.books_next_1_btn.place(x=550, y=770)
 
         self.books_next_5_btn = ttk.Button(self.root, text=">>", command=lambda: self.books_increment_selected_record_index(5))
-        self.books_next_5_btn.pack()
+        self.books_next_5_btn.place(x=620, y=770)
 
         self.books_next_all_btn = ttk.Button(self.root, text=">|", command=lambda: self.books_increment_selected_record_index(len(self.books_records) - self.books_selected_record_index))
-        self.books_next_all_btn.pack()
+        self.books_next_all_btn.place(x=690, y=770)
 
         # CRUD
 
         self.books_insert_btn = ttk.Button(self.root, text="Insert", command=self.books_insert_record)
-        self.books_insert_btn.pack()
+        self.books_insert_btn.place(x=430, y=800)
 
         self.books_update_btn = ttk.Button(self.root, text="Update", command=self.books_update_current_record)
-        self.books_update_btn.pack()
+        self.books_update_btn.place(x=500, y=800)
 
         self.books_delete_btn = ttk.Button(self.root, text="Delete", command=self.books_delete_current_record)
-        self.books_delete_btn.pack()
+        self.books_delete_btn.place(x=570, y=800)
 
 
     def books_increment_selected_record_index(self, amt):
